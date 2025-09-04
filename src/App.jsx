e27067f6792e4acb775ebdcd4505be3a21c2d15b
Shailesh <shaileshruns1995@gmail.com>
2025-09-04 15:53:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div className='text-red-100'>
+        aaaaaaaaaaaaaaaaaaaaaa
+      </div>
+    </>
+  )
+}
+
+export default App
